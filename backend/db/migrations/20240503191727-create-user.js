ae33d4735e5ad5e7ae59717365e65a331bfc125c
Chris W <97860788+AGiggleSniffer@users.noreply.github.com>
2024-05-04 12:55:00 -0700

    migrations need schema options :)

--- backend/db/migrations/20240503191727-create-user.js
+++ backend/db/migrations/20240503191727-create-user.js
@@ -1,4 +1,10 @@
 "use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+	options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
@@ -27,7 +33,6 @@ module.exports = {
 				type: Sequelize.DATE,
 			},
 		});
-		console.log("Migration Done!");
 	},
 	async down(queryInterface, Sequelize) {
 		await queryInterface.dropTable("Users");
